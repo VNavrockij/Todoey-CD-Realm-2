bb05e6e4e8a1093093958367640cc576e6b9a199
VNavrockij <vitalik.nav@gmail.com>
2023-06-13 12:08:48 +0300

    feat: add button for new item

--- Todoey-CD-Realm-2/TodoListViewController.swift
+++ Todoey-CD-Realm-2/TodoListViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    let itemArray = ["Hello", "World", "Again"]
+    var itemArray = ["Hello", "World", "Again"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,6 +30,13 @@ class TodoListViewController: UITableViewController {
     }
     
     
+    
+    //MARK: - Add button
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        print("Ok")
+        itemArray.append("new item")
+        tableView.reloadData()
+    }
     
 }
 
