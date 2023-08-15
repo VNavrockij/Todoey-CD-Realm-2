6dcb38f6997a0c7905b2edd5cd8bf014b3eedcb9
VNavrockij <vitalik.nav@gmail.com>
2023-08-15 15:59:34 +0300

    add: new TableView for categories

---
+++ Todoey-CD-Realm-2/Controllers/CategoryTableViewController.swift
@@ -0,0 +1,33 @@
+//
+//  CategoryTableViewController.swift
+//  Todoey-CD-Realm-2
+//
+//  Created by Vitalii Navrotskyi on 15.08.2023.
+//
+
+import UIKit
+import CoreData
+
+class CategoryTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    //MARK: - TableView Datasource Methods
+    
+    
+    //MARK: - TableView Manipulations Methods
+    
+    
+    
+    //MARK: - TableView Delegate Methods
+    
+    
+    
+}
