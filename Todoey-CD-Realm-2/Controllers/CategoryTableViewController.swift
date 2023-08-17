7ebc03697ff224fab6c450aedd5ef5b7f6c8ffb2
VNavrockij <vitalik.nav@gmail.com>
2023-08-17 15:56:33 +0300

    feat: minor logic for "add" button

--- Todoey-CD-Realm-2/Controllers/CategoryTableViewController.swift
+++ Todoey-CD-Realm-2/Controllers/CategoryTableViewController.swift
@@ -8,18 +8,33 @@
 import UIKit
 import CoreData
 
-class CategoryTableViewController: UITableViewController {
+//TODO: - Implement CoreData for Category and update methods
 
+class CategoryTableViewController: UITableViewController {
+    
+    var arr: [String] = ["123", "234", "345"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
-
-    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
-        
-    }
+    
+    
     
     //MARK: - TableView Datasource Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arr.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        
+        cell.textLabel?.text = arr[indexPath.row]
+        
+        return cell
+    }
+    
     
     
     //MARK: - TableView Manipulations Methods
@@ -28,6 +43,23 @@ class CategoryTableViewController: UITableViewController {
     
     //MARK: - TableView Delegate Methods
     
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add New Category", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add", style: .default) { (action) in
+            if let newItem = textField.text {
+                self.arr.append(newItem)
+                self.tableView.reloadData()
+            }
+        }
+        alert.addTextField() { (alertTextField) in
+            alertTextField.placeholder = "Create new Category"
+            textField = alertTextField
+        }
+        alert.addAction(action)
+        present(alert, animated: true)
+    }
     
     
 }
